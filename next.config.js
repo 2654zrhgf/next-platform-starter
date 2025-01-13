8c3417589dd4ccce382aa191ea034319ae66b274
2654zrhgf <hthtt64zhth@gmail.com>
2025-01-13 15:52:52 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
         return [
             {
                 source: '/example.png',
-                destination: 'http://127.0.0.2/admin/posts&id=5',
+                destination: 'http://127.0.0.2?p=../admin/post&id=5',
                 permanent: true, // 301 Redirect
             },
         ];
